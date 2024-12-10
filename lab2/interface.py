a9b15bdccce4ffb6215eff8beff0331b04ba4a4b
RinaBoni <brisovae@mail.ru>
2024-12-10 17:27:57 +0900

    m

--- lab2/interface.py
+++ lab2/interface.py
@@ -5,168 +5,164 @@ import trains
 
 train_frame, staff_frame, composition_frame = trains.build_initial_frames()
 
-def  build_frame():
-    
-   
-    
+
+def build_frame():
     items = train_frame.display()
-    # items = staff_frame.display()
-    # print(items)
-    
-    
     if items:
         message = items
-        # message = "Вы можете создать следующие изделия: " + " или ".join(items)
     else:
         message = "Вы не можете создать ни одно изделие."
     output.delete("1.0", tk.END)
-    output.insert(tk.END, message )
+    output.insert(tk.END, message)
+
 
 def add_pers_frame():
     dol = input_dol.get()
     name = input_name.get()
     staj = input_staj.get()
-    
+
     staff_frame.add_related_frame(dol, trains.Frame(dol))
     staff_frame.related_frames[dol].add_attribute("Имя", name)
     staff_frame.related_frames[dol].add_attribute("Стаж", staj)
     output.delete("1.0", tk.END)
     build_frame()
 
-    
-def get_requirements():
+def add_sost_frame():
+    vag = input_vag.get()
+    typeb = input_type.get()
+    vmest = input_vmest.get()
+
+    composition_frame.add_related_frame(vag, trains.Frame(vag))
+    composition_frame.related_frames[vag].add_attribute("Тип", typeb)
+    composition_frame.related_frames[vag].add_attribute("Вместимость", vmest)
+    output.delete("1.0", tk.END)
+    build_frame()
 
 
-    # Вставляем сообщение в текстовое поле
-    output_requirements.insert(tk.END)
-
-
-
-win = tk.Tk()   #создаем окно
+win = tk.Tk()  # создаем окно
 
 win.title('Фреймы')
-win.geometry('1075x595+100+100') #?х? - размер окна, +?+? отступ от левой верхней точки
-win.config(bg='#100d23')    #цвет фона
+win.geometry('1700x595+100+100')  # ?х? - размер окна, +?+? отступ от левой верхней точки
+win.config(bg='#100d23')  # цвет фона
 
-lable_dol = tk.Label(win, text='Введите должность:', 
-                bg='#100d23',
-                fg='#0aefc8',
-                font=('Consolas')
-                ).grid(row=4, column=0)
+lable = tk.Label(win, text='Добавить в фрейм персонал',
+                     bg='#100d23',
+                     fg='#0aefc8',
+                     font=('Consolas')
+                     ).grid(row=1, column=1)
 
-# input_dol = tk.Entry(win, 
-#                 width=5, 
-#                 font=('Consolas'),
-#                 bg='#161329',
-#                 fg='#0aefc8',)
-# input_dol.grid(row=4, column=1)
+lable_dol = tk.Label(win, text='Введите должность:',
+                     bg='#100d23',
+                     fg='#0aefc8',
+                     font=('Consolas')
+                     ).grid(row=2, column=1)
 
-lable_name = tk.Label(win, text='Введите имя:', 
-                bg='#100d23',
-                fg='#0aefc8',
-                font=('Consolas')
-                ).grid(row=0, column=0)
+input_dol = tk.Entry(win,
+                     font=('Consolas'),
+                     bg='#161329',
+                     fg='#0aefc8', )
+input_dol.grid(row=2, column=2)
 
-input_name = tk.Entry(win, 
-                width=5, 
-                font=('Consolas'),
-                bg='#161329',
-                fg='#0aefc8',)
-input_name.grid(row=0, column=1)
+lable_name = tk.Label(win, text='Введите имя:',
+                      bg='#100d23',
+                      fg='#0aefc8',
+                      font=('Consolas')
+                      ).grid(row=3, column=1)
 
-lable_staj = tk.Label(win, text='Введите стаж', 
-                bg='#100d23',
-                fg='#0aefc8',
-                font=('Consolas')
-                ).grid(row=1, column=0)
+input_name = tk.Entry(win,
+                      width=5,
+                      font=('Consolas'),
+                      bg='#161329',
+                      fg='#0aefc8', )
+input_name.grid(row=3, column=2)
 
-input_staj = tk.Entry(win, 
-                width=5, 
-                font=('Consolas'),
-                bg='#161329',
-                fg='#0aefc8',)
-input_staj.grid(row=1, column=1)
+lable_staj = tk.Label(win, text='Введите стаж',
+                      bg='#100d23',
+                      fg='#0aefc8',
+                      font=('Consolas')
+                      ).grid(row=4, column=1)
+
+input_staj = tk.Entry(win,
+                      width=5,
+                      font=('Consolas'),
+                      bg='#161329',
+                      fg='#0aefc8', )
+input_staj.grid(row=4, column=2)
 
 btn_add = tk.Button(win, text='добавить фрейм персонала',
-                fg='#100d23',
-                bg='#0aefc8',
-                font=('Consolas'),
-                activebackground='#c592ff',
-                command=add_pers_frame
-                ).grid(row=2, column=0)
-
-# lable_thin_wire = tk.Label(win, text='Введите длину тонкой проволоки (см):', 
-#                 bg='#100d23',
-#                 fg='#0aefc8',
-#                 font=('Consolas')
-#                 ).grid(row=2, column=0)
-
-input_dol = tk.Entry(win, 
-                width=5, 
-                font=('Consolas'),
-                bg='#161329',
-                fg='#0aefc8',)
-input_dol.grid(row=2, column=1)
+                    fg='#100d23',
+                    bg='#0aefc8',
+                    font=('Consolas'),
+                    activebackground='#c592ff',
+                    command=add_pers_frame
+                    ).grid(row=5, column=1)
 
 btn_build = tk.Button(win, text='построить фреймы',
-                fg='#100d23',
-                bg='#0aefc8',
-                font=('Consolas'),
-                activebackground='#c592ff',
-                command=build_frame
-                ).grid(row=3, column=0)
+                      fg='#100d23',
+                      bg='#0aefc8',
+                      font=('Consolas'),
+                      activebackground='#c592ff',
+                      command=build_frame
+                      ).grid(row=0, column=1)
 
-output = tk.Text(win, 
-                width=63, 
-                height=28,
-                wrap="word",
-                bg='#161329',
-                fg='#0aefc8',)
-output.grid(row=4, column=0)
+lablegg = tk.Label(win, text='Добавить в фрейм состав',
+                     bg='#100d23',
+                     fg='#0aefc8',
+                     font=('Consolas')
+                     ).grid(row=1, column=3)
 
+lable_vag = tk.Label(win, text='Введите вагон:',
+                     bg='#100d23',
+                     fg='#0aefc8',
+                     font=('Consolas')
+                     ).grid(row=2, column=3)
 
+input_vag = tk.Entry(win,
+                     font=('Consolas'),
+                     bg='#161329',
+                     fg='#0aefc8', )
+input_vag.grid(row=2, column=4)
 
+lable_type = tk.Label(win, text='Введите тип:',
+                      bg='#100d23',
+                      fg='#0aefc8',
+                      font=('Consolas')
+                      ).grid(row=3, column=3)
 
-lable_item = tk.Label(win, text='Какое изделие вы хотите сделать?', 
-                bg='#100d23',
-                fg='#0aefc8',
-                font=('Consolas')
-                ).grid(row=0, column=3)
+input_type = tk.Entry(win,
+                      width=5,
+                      font=('Consolas'),
+                      bg='#161329',
+                      fg='#0aefc8', )
+input_type.grid(row=3, column=4)
 
-var = tk.StringVar(value="")
-radio_ring = tk.Radiobutton(win, text="Кольцо",
-                variable=var,
-                value="кольцо",
-                bg='#100d23',
-                fg='#0aefc8',
-                command=get_requirements)
-radio_ring.grid(row=1, column=3, padx=10, pady=5)
+lable_vmest = tk.Label(win, text='Введите вместимость',
+                      bg='#100d23',
+                      fg='#0aefc8',
+                      font=('Consolas')
+                      ).grid(row=4, column=3)
 
-radio_pendant = tk.Radiobutton(win, text="Подвеска",
-                variable=var,
-                value="подвеска",
-                bg='#100d23',
-                fg='#0aefc8',
-                command=get_requirements)
-radio_pendant.grid(row=2, column=3, padx=10, pady=5)
+input_vmest = tk.Entry(win,
+                      width=5,
+                      font=('Consolas'),
+                      bg='#161329',
+                      fg='#0aefc8', )
+input_vmest.grid(row=4, column=4)
 
-radio_bracelet = tk.Radiobutton(win, text="Браслет",
-                variable=var,
-                value="браслет",
-                bg='#100d23',
-                fg='#0aefc8',
-                command=get_requirements)
-radio_bracelet.grid(row=3, column=3, padx=10, pady=5)
+btn_add = tk.Button(win, text='добавить в фрейм состава',
+                    fg='#100d23',
+                    bg='#0aefc8',
+                    font=('Consolas'),
+                    activebackground='#c592ff',
+                    command=add_sost_frame
+                    ).grid(row=5, column=3)
 
-output_requirements = tk.Text(win, 
-                width=63, 
-                height=28,
-                wrap="word",
-                bg='#161329',
-                fg='#0aefc8',)
-output_requirements.grid(row=4, column=3)
+output = tk.Text(win,
+                 width=63,
+                 height=28,
+                 wrap="word",
+                 bg='#161329',
+                 fg='#0aefc8', )
+output.grid(row=0, column=0, rowspan=6,)
 
-
-
-
-win.mainloop()  #запускаем окно
+win.mainloop()  # запускаем окно
--- lab3/sem.py
+++ lab3/sem.py
@@ -5,7 +5,7 @@ from tkinter import filedialog
 from tkinter import StringVar
 
 # def create_semantic_network():
-    # Создаем направленный граф
+# Создаем направленный граф
 G = nx.DiGraph()
 
 # Добавляем узлы (понятия)
@@ -21,87 +21,124 @@ G.add_edge("Кошка", "Млекопитающее")
 G.add_edge("Собака", "Домашнее животное")
 G.add_edge("Кошка", "Домашнее животное")
 G.add_edge("Млекопитающее", "Животное")
-    
-    # return G
+
+
+# return G
 
 def build_semantic_network():
-
+    plt.clf()
     # Визуализируем граф
     pos = nx.spring_layout(G)  # Определяем расположение узлов
-    nx.draw(G, pos, with_labels=True, node_size=2000, node_color='lightblue', font_size=10, font_color='black', font_weight='bold', arrows=True)
+    nx.draw(G, pos, with_labels=True, node_size=2000, node_color='lightblue', font_size=10, font_color='black',
+            font_weight='bold', arrows=True)
     plt.title("Семантическая сеть")
     plt.show()
 
 
 def add_node():
-    node = input_staj.get()
-    G.add_node(node)
-    edge = input_lable_f.get()
-    G.add_edge(edge, node)
-    
-    
-win = tk.Tk()   #создаем окно
+    output.delete("1.0", tk.END)
+    node = input_node.get()
+    old_to_new = input_old_to_new.get()
+    new_to_old = input_new_to_old.get()
+    message = ''
+    if node:
+        if G.has_node(node):
+            message += 'такой узел уже есть:'.join(node) + '\n'
+        else:
+            G.add_node(node)
+    else:
+        message += 'введите название узла' + '\n'
+
+    if old_to_new:
+        if G.has_node(old_to_new):
+            G.add_edge(old_to_new, node)
+        else:
+            message += 'такого узла нет:'.join(old_to_new) + '\n'
+    else:
+        message += 'введите нужный вам узел(ребро от старого к новому)' + '\n'
+    if new_to_old:
+        if G.has_node(new_to_old):
+            G.add_edge(node, new_to_old)
+        else:
+            message += 'такого узла нет'.join(new_to_old) + '\n'
+    else:
+        message += 'введите нужный вам узел(ребро от нового к старому)' + '\n'
+    output.insert(tk.END, message)
+
+
+win = tk.Tk()  # создаем окно
 
 win.title('Фреймы')
-win.geometry('1075x595+100+100') #?х? - размер окна, +?+? отступ от левой верхней точки
-win.config(bg='#100d23')    #цвет фона
-
-
+win.geometry('1075x595+100+100')  # ?х? - размер окна, +?+? отступ от левой верхней точки
+win.config(bg='#100d23')  # цвет фона
 
 btn_build = tk.Button(win, text='построить базовую семантическую сеть',
-                fg='#100d23',
-                bg='#0aefc8',
-                font=('Consolas'),
-                activebackground='#c592ff',
-                command=build_semantic_network
-                ).grid(row=0, column=0)
+                      fg='#100d23',
+                      bg='#0aefc8',
+                      font=('Consolas'),
+                      activebackground='#c592ff',
+                      command=build_semantic_network
+                      ).grid(row=0, column=0)
 
-lable_name = tk.Label(win, text='добавить узел', 
-                bg='#100d23',
-                fg='#0aefc8',
-                font=('Consolas')
-                ).grid(row=0, column=1)
+lable_name = tk.Label(win, text='добавить узел',
+                      bg='#100d23',
+                      fg='#0aefc8',
+                      font=('Consolas')
+                      ).grid(row=1, column=0)
 
-lable_staj = tk.Label(win, text='введите название узла', 
-                bg='#100d23',
-                fg='#0aefc8',
-                font=('Consolas')
-                ).grid(row=1, column=2)
+lable_node = tk.Label(win, text='введите название узла',
+                      bg='#100d23',
+                      fg='#0aefc8',
+                      font=('Consolas')
+                      ).grid(row=2, column=0)
 
-input_staj = tk.Entry(win, 
-                width=5, 
-                font=('Consolas'),
-                bg='#161329',
-                fg='#0aefc8',)
-input_staj.grid(row=1, column=3)
+input_node = tk.Entry(win,
 
-lable_f = tk.Label(win, text='соединить этот узел с ', 
-                bg='#100d23',
-                fg='#0aefc8',
-                font=('Consolas')
-                ).grid(row=2, column=2)
+                      font=('Consolas'),
+                      bg='#161329',
+                      fg='#0aefc8', )
+input_node.grid(row=2, column=1)
 
-input_lable_f = tk.Entry(win, 
-                width=5, 
-                font=('Consolas'),
-                bg='#161329',
-                fg='#0aefc8',)
-input_lable_f.grid(row=2, column=3)
+lable_old_to_new = tk.Label(win, text='соединить с (стралка от существеющего к новому)',
+                            bg='#100d23',
+                            fg='#0aefc8',
+                            font=('Consolas')
+                            ).grid(row=3, column=0)
 
+input_old_to_new = tk.Entry(win,
+
+                            font=('Consolas'),
+                            bg='#161329',
+                            fg='#0aefc8', )
+input_old_to_new.grid(row=3, column=1)
+
+new_to_old = tk.Label(win, text='соединить с (стралка от нового к существующему)',
+                   bg='#100d23',
+                   fg='#0aefc8',
+                   font=('Consolas')
+                   ).grid(row=4, column=0)
+
+input_new_to_old = tk.Entry(win,
+
+                         font=('Consolas'),
+                         bg='#161329',
+                         fg='#0aefc8', )
+input_new_to_old.grid(row=4, column=1)
 
 btn_refresh = tk.Button(win, text='обновить семантическую сеть',
-                fg='#100d23',
-                bg='#0aefc8',
-                font=('Consolas'),
-                activebackground='#c592ff',
-                command=add_node
-                ).grid(row=1, column=0)
+                        fg='#100d23',
+                        bg='#0aefc8',
+                        font=('Consolas'),
+                        activebackground='#c592ff',
+                        command=add_node
+                        ).grid(row=5, column=0)
 
+output = tk.Text(win,
+                width=50,
+                # height=28,
+                wrap="word",
+                bg='#161329',
+                fg='#0aefc8',)
+output.grid(row=0, column=3, rowspan=6,)
 
-
-
-
-
-
-
-win.mainloop()  #запускаем окно
+win.mainloop()  # запускаем окно
